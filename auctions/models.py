87332f4a58f257a5fc8fdcecb2509f496056a067
mWhitman189 <mwhitman189@gmail.com>
2020-09-10 19:34:35 +0900

    refactor: Move categories select choices to model

--- auctions/models.py
+++ auctions/models.py
@@ -29,8 +29,41 @@ class Rating(models.Model):
 
 
 class AuctionListing(models.Model):
+    BOOKS = 'BKS'
+    BUSINESS_N_INDUSTRIAL = 'BNI'
+    CLOTHING_SHOES_N_ACCESSORIES = 'CSE'
+    COLLECTIBLES = 'COL'
+    CONSUMER_ELECTRONICS = 'CEL'
+    CRAFTS = 'CFT'
+    HOME_N_GARDEN = 'HNG'
+    MOTORS = 'MTR'
+    PET_SUPPLIES = 'PTS'
+    SPORTING_GOODS = 'SPT'
+    SPORTS_MEM = 'SPM'
+    TOYS_N_HOBBIES = 'TNH'
+    ANTIQUES = 'ATQ'
+    COMPUTERS_N_NETWORKING = 'CNN'
+
+    CATEGORY_CHOICES = [
+        ("", "--Category--"),
+        ("BKS", "Books"),
+        ("BNI", "Business & Industrial"),
+        ("CSE", "Clothing, Shoes & Accessories"),
+        ("COL", "Collectibles"),
+        ("CEL", "Consumer Electronics"),
+        ("CFT", "Crafts"),
+        ("HNG", "Home & Garden"),
+        ("MTR", "Motors"),
+        ("PTS", "Pet Supplies"),
+        ("SPT", "Sporting Goods"),
+        ("SPM", "Sports Mem, Cards & Fan Shop"),
+        ("TNH", "Toys & Hobbies"),
+        ("ATQ", "Antiques"),
+        ("CNN", "Computers/Tablets & Networking"),
+    ]
+
     name = models.CharField(max_length=64)
-    category = models.CharField(max_length=64)
+    category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
     starting_bid = models.DecimalField(max_digits=13, decimal_places=2)
     buyout_price = models.DecimalField(max_digits=13, decimal_places=2)
     details = models.TextField(max_length=400)
