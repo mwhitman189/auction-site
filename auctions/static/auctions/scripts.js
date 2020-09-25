b836f6e6e880a59bbe55372196e42d8ec9debab4
mWhitman189 <mwhitman189@gmail.com>
2020-09-25 09:20:17 +0900

    fix: Add error message displays using django messages

--- auctions/static/auctions/scripts.js
+++ auctions/static/auctions/scripts.js
@@ -1,1 +1,12 @@
-watchlistBtn = document.querySelector('#watchlist-btn')
+document.addEventListener('DOMContentLoaded', function () {
+    const rmvBtn = document.querySelector('.remove-btn')
+    rmvBtn.style.animationPlayState = 'paused'
+
+    document.querySelector('a').onclick = () => {
+        if (rmvBtn.style.animationPlayState === 'paused') {
+            rmvBtn.style.animationPlayState = 'running'
+        } else {
+            rmvBtn.style.animationPlayState = 'paused'
+        }
+    }
+})
--- auctions/views.py
+++ auctions/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -13,7 +14,7 @@ from .models import User, AuctionListing, Bid, WatchList, Comment
 def index(request):
     listings = AuctionListing.objects.filter(is_active=True)
 
-    return render(request, "auctions/index.html", {"listings": listings})
+    return render(request, 'auctions/index.html', {'listings': listings})
 
 
 def listing(request, listing_id):
@@ -33,7 +34,7 @@ def listing(request, listing_id):
     else:
         current_bid = listing.starting_bid
 
-    if request.method == "POST":
+    if request.method == 'POST':
         form = NewBidForm(request.POST)
         if not Bid.objects.filter(listing=listing_id, is_active=True).exists():
             bid_object = form.save(commit=False)
@@ -42,16 +43,17 @@ def listing(request, listing_id):
 
         if form.is_valid():
             # If the current existing bid is greater than the input amount, return an error
-            if current_bid > form.cleaned_data["amount"]:
-                return render(request, "auctions/listing.html", {
-                    "listing": listing,
-                    "current_bid": current_bid,
-                    "comments": comments,
-                    "form": form,
-                    "error": "Bid must be higher than current bid"
+            if current_bid > form.cleaned_data['amount']:
+                messages.error(
+                    request, "Your bid must be higher than the current bid")
+                return render(request, 'auctions/listing.html', {
+                    'listing': listing,
+                    'current_bid': current_bid,
+                    'comments': comments,
+                    'form': form
                 })
             else:
-                current_bid = form.cleaned_data["amount"]
+                current_bid = form.cleaned_data['amount']
                 # Deactivate old bids
                 bid_object.is_active = False
                 bid_object.save()
@@ -61,28 +63,28 @@ def listing(request, listing_id):
                 bid.bidder = request.user
                 bid.save()
 
-                return render(request, "auctions/listing.html", {
-                    "listing": listing,
-                    "bid": bid.amount,
-                    "comments": comments,
-                    "is_on_watchlist": is_on_watchlist,
-                    "form": NewBidForm()
+                return render(request, 'auctions/listing.html', {
+                    'listing': listing,
+                    'bid': bid.amount,
+                    'comments': comments,
+                    'is_on_watchlist': is_on_watchlist,
+                    'form': NewBidForm()
                 })
         else:
-            return render(request, "auctions/listing.html", {
-                "listing": listing,
-                "bid": current_bid,
-                "comments": comments,
-                "is_on_watchlist": is_on_watchlist,
-                "form": form
+            return render(request, 'auctions/listing.html', {
+                'listing': listing,
+                'bid': current_bid,
+                'comments': comments,
+                'is_on_watchlist': is_on_watchlist,
+                'form': form
             })
 
-    return render(request, "auctions/listing.html", {
-        "listing": listing,
-        "bid": current_bid,
-        "comments": comments,
-        "is_on_watchlist": is_on_watchlist,
-        "form": NewBidForm()
+    return render(request, 'auctions/listing.html', {
+        'listing': listing,
+        'bid': current_bid,
+        'comments': comments,
+        'is_on_watchlist': is_on_watchlist,
+        'form': NewBidForm()
     })
 
 
@@ -94,13 +96,15 @@ def listing_closeout(request, listing_id):
 
     try:
         highest_bid = Bid.objects.get(listing=listing, is_active=True)
-    except Bid.DoesNotExist:
-        highest_bid = None
+        if request.user.id == listing.seller.id:
+            listing.is_active = False
+            highest_bid.is_winner = True
+            highest_bid.is_active = False
 
-    if request.user.id == listing.seller.id:
-        listing.is_active = False
-        highest_bid.is_winner = True
-        highest_bid.is_active = False
+    except Bid.DoesNotExist:
+        messages.error(request, "There are no bids on this item")
+        return HttpResponseRedirect(reverse('auctions:listing', args={listing_id}))
+
     return False
 
 
@@ -108,7 +112,7 @@ def new_listing(request):
     """
     Return a form to create a new listing
     """
-    if request.method == "POST":
+    if request.method == 'POST':
         form = NewListingForm(request.POST)
 
         if form.is_valid():
@@ -116,13 +120,13 @@ def new_listing(request):
             listing.seller = request.user
             listing.save()
 
-            return HttpResponseRedirect(reverse("auctions:listing", args={listing.id}))
+            return HttpResponseRedirect(reverse('auctions:listing', args={listing.id}))
 
         else:
-            return render(request, "auctions/new_listing.html", {"form": form})
+            return render(request, 'auctions/new_listing.html', {'form': form})
 
     else:
-        return render(request, "auctions/new_listing.html", {"form": NewListingForm()})
+        return render(request, 'auctions/new_listing.html', {'form': NewListingForm()})
 
 
 def watchlist(request):
@@ -131,12 +135,12 @@ def watchlist(request):
     """
     watchlist = WatchList.objects.filter(user=request.user)
 
-    return render(request, "auctions/watchlist.html", {"watchlist": watchlist})
+    return render(request, 'auctions/watchlist.html', {'watchlist': watchlist})
 
 
 def watchlist_toggle(request, listing_id):
     """
-    Add a listing to the user's watchlist
+    Add listing to user's watchlist
     """
     user = request.user
     listing = AuctionListing.objects.get(id=listing_id)
@@ -149,7 +153,7 @@ def watchlist_toggle(request, listing_id):
 
         watchlist_item.save()
 
-    return HttpResponseRedirect(reverse("auctions:listing", args={listing_id}))
+    return HttpResponseRedirect(reverse('auctions:listing', args={listing_id}))
 
 
 def categories(request):
@@ -165,7 +169,7 @@ def categories(request):
         entry['category_abr'] = force_text(
             choices[entry['category']], strings_only=True)
 
-    return render(request, "auctions/categories.html", {"categories": category_counts})
+    return render(request, 'auctions/categories.html', {'categories': category_counts})
 
 
 def category(request, category):
@@ -175,7 +179,7 @@ def category(request, category):
     category_items = AuctionListing.objects.filter(
         category=category, is_active=True)
 
-    return render(request, "auctions/category.html", {"category": category, "category_items": category_items})
+    return render(request, 'auctions/category.html', {'category': category, 'category_items': category_items})
 
 
 def myItems(request):
@@ -186,52 +190,49 @@ def myItems(request):
 
 
 def login_view(request):
-    if request.method == "POST":
+    if request.method == 'POST':
 
         # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
 
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("auctions:index"))
+            return HttpResponseRedirect(reverse('auctions:index'))
         else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            messages.error(request, "Invalid username and/or password")
+            return render(request, 'auctions/login.html')
     else:
-        return render(request, "auctions/login.html")
+        return render(request, 'auctions/login.html')
 
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("auctions:index"))
+    return HttpResponseRedirect(reverse('auctions:index'))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
 
         # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
         if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+            messages.error(request, "Passwords must match")
+            return render(request, 'auctions/register.html')
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
+            messages.error(request, "That username is already taken")
+            return render(request, 'auctions/register.html')
         login(request, user)
-        return HttpResponseRedirect(reverse("auctions:index"))
+        return HttpResponseRedirect(reverse('auctions:index'))
     else:
-        return render(request, "auctions/register.html")
+        return render(request, 'auctions/register.html')
